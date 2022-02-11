fe3e58d3e5d9333fa1583ff5599967092ff0d1c5
Ipuch <puchaud.pierre@gmail.com>
2022-02-10 23:26:52 -0500

    maj jm, need to fix the database for some surveys.

--- interface_mj.py
+++ interface_mj.py
@@ -1,4 +1,4 @@
-from libs import majority_judgment as mj
+from libs.majority_judgment_2 import majority_judgment as mj
 import pandas as pd
 from pandas import DataFrame
 from utils import (
@@ -12,7 +12,7 @@ def sort_candidates_mj(
     nb_grades: int,
 ):
     """
-    Reindexing candidates in the dataFrame following mj rules
+    Reindexing candidates in the dataFrame following majority judgment rules
 
     Parameters
     ----------
@@ -32,22 +32,32 @@ def sort_candidates_mj(
 
     # only intentions of votes in the dataframe
     intentions = df[col]
-    merit_profiles = generate_merit_profiles(intentions, nb_grades, nb_candidates)
 
-    # generate data for the use of mj
-    majo_vals = [mj.MajorityValue(profil) for profil in merit_profiles]
-    sorted_majo_vals = mj.sort_by_value_with_index(majo_vals)
+    # merit_profiles1 = set_merit_profiles(intentions, nb_grades, nb_candidates)
+    # merit_profiles2 = set_merit_profiles2(intentions, nb_grades, nb_candidates)
+    # merit_profiles3 = set_merit_profiles3(intentions, nb_grades, nb_candidates)
 
+    # # generate data for the use of mj
+    # majo_vals = [mj.MajorityValue(profil) for profil in merit_profiles1]
+    # sorted_majo_vals = mj.sort_by_value_with_index(majo_vals)
     # get back index of ranks for each candidates
-    rank_idx = [idx for idx, value in sorted_majo_vals]
+    # rank_idx = [idx for idx, value in sorted_majo_vals]
+    # rank_idx.reverse()
 
+    merit_profiles_dict = set_dictionary(df_intentions, nb_grades, nb_candidates)
+    ranking = mj(merit_profiles_dict, reverse=True)
+
+    new_df = pd.DataFrame(columns=df_intentions.columns)
+    for key in ranking:
+        row = df_intentions[df_intentions["candidat"] == key]
+        new_df = pd.concat([new_df, row], ignore_index=True)
     # set new index of rows
-    df_intentions.index = pd.Index(data=rank_idx, dtype="int64")
+    new_df.index = pd.Index(data=[i for i in range(1, nb_candidates+1)], dtype="int64")
 
-    return df_intentions.sort_index(axis=0, ascending=True)
+    return df_intentions.reindex(index=df_intentions.index[::-1])
 
 
-def generate_merit_profiles(df_intentions: DataFrame, nb_grades: int, nb_candidates: int):
+def set_merit_profiles(df_intentions: DataFrame, nb_grades: int, nb_candidates: int):
     """
     Convert a list of votes into a matrix containing the number of grades for
     each candidate
@@ -66,3 +76,45 @@ def generate_merit_profiles(df_intentions: DataFrame, nb_grades: int, nb_candida
     the length of the list is the number of candidates
     """
     return [{j: df_intentions.iloc[i, j] for j in range(nb_grades)} for i in range(nb_candidates)]
+
+
+def set_merit_profiles2(df_intentions: DataFrame, nb_grades: int, nb_candidates: int):
+    """
+    Convert a list of votes into a matrix containing the number of grades for
+    each candidate
+
+    Parameters
+    ----------
+    df_intentions: DataFrame
+        contains only all votes for each grade
+    nb_grades: int
+        number of grades
+    nb_candidates: int,
+        number of candidates
+    Returns
+    -------
+    A list of dictionaries which contains the number of votes for each grade for each candidates,
+    the length of the list is the number of candidates
+    """
+    return [{j: df_intentions.iloc[i, nb_grades-j-1] for j in range(nb_grades-1, -1, -1)} for i in range(nb_candidates)]
+
+
+def set_dictionary(df_intentions: DataFrame, nb_grades: int, nb_candidates: int):
+    """
+    Convert a list of votes into a matrix containing the number of grades for
+    each candidate
+
+    Parameters
+    ----------
+    df_intentions: DataFrame
+        contains only all votes for each grade
+    nb_grades: int
+        number of grades
+    nb_candidates: int,
+        number of candidates
+    Returns
+    -------
+    A list of dictionaries which contains the number of votes for each grade for each candidates,
+    the length of the list is the number of candidates
+    """
+    return {df_intentions["candidat"].iloc[i]: [df_intentions.iloc[i, j+1] for j in range(nb_grades)] for i in range(nb_candidates)}
